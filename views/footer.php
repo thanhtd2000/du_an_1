56a2ec432f690737326f7b9c57a407c4fb6c7832
thanhtd2000 <thanhhandgun2@gmail.com>
2022-07-24 21:17:27 +0700

    Update footer.php

--- views/footer.php
+++ views/footer.php
@@ -1,110 +1,108 @@
 <div class="footer">
-    <div class="footer__content container">
-        <div class="footer__top">
-            <div class="row">
-                <div class="col-3">
-                    <img src="./img/logo.png" alt="">
-                    <ul>
-                        <li><a href=""><i class="fa-solid fa-location-dot"></i>Đảo Hòn Tre,
-                                Phường Vĩnh Nguyên,
-                                Thành phố Nha Trang,
-                                Tỉnh Khánh Hòa, Việt Nam</a></li>
-                        <li><a href="" style="color: #f39f2d;"><i class="fa-solid fa-envelope"></i>VietnamExplorationTrips@gmail.com</a>
-                        </li>
-                        <li><a href="" style="color: #f39f2d; font-weight: 700;"><i class="fa-solid fa-phone"></i>1900 23 23 89 (Nhánh số 3)</a>
-                        </li>
-                    </ul>
-                    <img class="img-fluid" src="./img/logo-da-thong-bao-website-voi-bo-cong-thuong.webp" alt="">
-                </div>
-                <div class="col-3">
-                    <h5>GIỚI THIỆU</h5>
-                    <ul>
-                        <li><a href="">Về chúng tôi</a></li>
-                        <li><a href="">Thành tựu</a></li>
-                        <li><a href="">Liên hệ</a></li>
-                        <li><a href="">Tuyển dụng</a></li>
-                        <li><a href="">Câu hỏi thường gặp</a></li>
-                        <li><a href="">Bản đồ trang</a></li>
-                    </ul>
-                </div>
-                <div class="col-3">
-                    <h5>TIN TỨC & SỰ KIỆN</h5>
-                    <ul>
-                        <li><a href="">Tin công ty</a></li>
-                        <li><a href="">Cẩm nang du lịch</a></li>
-                    </ul>
-                    <h5>THƯ VIỆN ẢNH</h5>
-                </div>
-                <div class="col-3">
-                    <h5>ĐIỀU KHOẢN & QUY ĐỊNH</h5>
-                    <ul>
-                        <li><a href="">Điều khoản chung</a></li>
-                        <li><a href="">Quy định chung</a></li>
-                        <li><a href="">Quy định thanh toán</a></li>
-                        <li><a href="">Quy định về xác nhận thông tin đặt phòng</a></li>
-                        <li><a href="">Chính sách giỏi quyết tranh chấp</a></li>
-                        <li><a href="">Chính sách quyền riêng tư</a></li>
-                    </ul>
+        <div class="footer__content container">
+            <div class="footer__top">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="./img/logo.png" alt="">
+                        <ul>
+                            <li><a href=""><i class="fa-solid fa-location-dot"></i>Đảo Hòn Tre,
+                                    Phường Vĩnh Nguyên,
+                                    Thành phố Nha Trang,
+                                    Tỉnh Khánh Hòa, Việt Nam</a></li>
+                            <li><a href="" style="color: #f39f2d;"><i class="fa-solid fa-envelope"></i>VietnamExplorationTrips@gmail.com</a>
+                            </li>
+                            <li><a href="" style="color: #f39f2d; font-weight: 700;"><i class="fa-solid fa-phone"></i>1900 23 23 89 (Nhánh số 3)</a>
+                            </li>
+                        </ul>
+                        <img class="img-fluid" src="./img/logo-da-thong-bao-website-voi-bo-cong-thuong.webp" alt="">
+                    </div>
+                    <div class="col-3">
+                        <h5>GIỚI THIỆU</h5>
+                        <ul>
+                            <li><a href="">Về chúng tôi</a></li>
+                            <li><a href="">Thành tựu</a></li>
+                            <li><a href="">Liên hệ</a></li>
+                            <li><a href="">Tuyển dụng</a></li>
+                            <li><a href="">Câu hỏi thường gặp</a></li>
+                            <li><a href="">Bản đồ trang</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-3">
+                        <h5>TIN TỨC & SỰ KIỆN</h5>
+                        <ul>
+                            <li><a href="">Tin công ty</a></li>
+                            <li><a href="">Cẩm nang du lịch</a></li>
+                        </ul>
+                        <h5>THƯ VIỆN ẢNH</h5>
+                    </div>
+                    <div class="col-3">
+                        <h5>ĐIỀU KHOẢN & QUY ĐỊNH</h5>
+                        <ul>
+                            <li><a href="">Điều khoản chung</a></li>
+                            <li><a href="">Quy định chung</a></li>
+                            <li><a href="">Quy định thanh toán</a></li>
+                            <li><a href="">Quy định về xác nhận thông tin đặt phòng</a></li>
+                            <li><a href="">Chính sách giỏi quyết tranh chấp</a></li>
+                            <li><a href="">Chính sách quyền riêng tư</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="footer__mid">
-            <ul>
-                <li><a href=""><i class="fa-brands fa-facebook-f"></i></a></li>
-                <li><a href=""><i class="fa-brands fa-youtube"></i></a></li>
-                <li><a href=""><i class="fa-brands fa-instagram"></i></a></li>
-            </ul>
-            <div class="divsion"></div>
-        </div>
-        <div class="footer__bottom">
-            <div class="row">
-                <div class="col-3">
-                    <h6>Số ĐKKD:</h6>
-                    <p style="width: 80%;">4200456848. ĐK lần đầu 26/7/2006.</p>
-                    <p>ĐK thay đổi lần thứ 50: 3/3/2020</p>
-                </div>
-                <div class="col-6">
-                    <h6>Nơi cấp:</h6>
-                    <p>Sở kế hoạch và đầu tư tỉnh Khánh Hòa</p>
-                    <h6>Lĩnh vực kinh doanh:</h6>
-                    <p>Khách sạn, biệt thự hoặc căn hộ kinh doanh dịch vụ lưu trú ngắn ngày; khu du lịch sinh thái
-                    </p>
+            <div class="footer__mid">
+                <ul>
+                    <li><a href=""><i class="fa-brands fa-facebook-f"></i></a></li>
+                    <li><a href=""><i class="fa-brands fa-youtube"></i></a></li>
+                    <li><a href=""><i class="fa-brands fa-instagram"></i></a></li>
+                </ul>
+                <div class="divsion"></div>
+            </div>
+            <div class="footer__bottom">
+                <div class="row">
+                    <div class="col-3">
+                        <h6>Số ĐKKD:</h6>
+                        <p style="width: 80%;">4200456848. ĐK lần đầu 26/7/2006.</p>
+                        <p>ĐK thay đổi lần thứ 50: 3/3/2020</p>
+                    </div>
+                    <div class="col-6">
+                        <h6>Nơi cấp:</h6>
+                        <p>Sở kế hoạch và đầu tư tỉnh Khánh Hòa</p>
+                        <h6>Lĩnh vực kinh doanh:</h6>
+                        <p>Khách sạn, biệt thự hoặc căn hộ kinh doanh dịch vụ lưu trú ngắn ngày; khu du lịch sinh thái
+                        </p>
 
-                </div>
-                <div class="col-3">
-                    <h6>Chủ tài khoản:</h6>
-                    <p> Công ty cổ phần Vinpearl Tài khoản ngân hàng số: 9124412488166 (VND) Ngân hàng thương mại cổ phần Kỹ Thương Việt Nam (Techcombank) – Hội sở</p>
+                    </div>
+                    <div class="col-3">
+                        <h6>Chủ tài khoản:</h6>
+                        <p> Công ty cổ phần Vinpearl Tài khoản ngân hàng số: 9124412488166 (VND) Ngân hàng thương mại cổ phần Kỹ Thương Việt Nam (Techcombank) – Hội sở</p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="js/owl-carousel/owl.carousel.min.js"></script>
-<script>
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: false,
-        dots: false,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 3
-            },
-        }
-    })
-</script>
-<script src="js/main.js"></script>
-<script src="../js/backtotop/util.js"></script>
-<script src="../js/backtotop/mainbacktotop.js"></script>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="js/owl-carousel/owl.carousel.min.js"></script>
+    <script>
+        $('.owl-carousel').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: false,
+            dots: false,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 3
+                },
+            }
+        })
+    </script>
+    <script src="js/main.js"></script>
 </body>
 
 </html>
