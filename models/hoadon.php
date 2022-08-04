eba27cb28db9822599e29234c8097576b358ddfd
thanhtd2000 <thanhhandgun2@gmail.com>
2022-08-04 23:13:03 +0700

    Update hoadon.php

--- models/hoadon.php
+++ models/hoadon.php
@@ -1,6 +1,6 @@
 <?php
 function load_allhd($iduser){
-    $sql = "SELECT a.*,b.ngaydathang FROM donhang as a inner join bill as b on a.idbill = b.idbill where iduser =$iduser ORDER BY madh DESC ";
+    $sql = "SELECT a.*,b.ngaydathang FROM donhang as a inner join bill as b on a.idbill = b.id where iduser =$iduser ORDER BY madh DESC ";
     $listhd = pdo_query($sql);
     return $listhd;
 }
