1f8351840575d949a9b1468e8c39c23884d472fe
truong210120 <truongnguyen210120@gmail.com>
2022-08-12 10:04:09 +0700

    updatedas

--- views/home.php
+++ views/home.php
@@ -5,7 +5,7 @@
                         <div class="carousel-item active">
                               <img src="./img/Sea-Package-Homepage-Banner_1652346356.jpg.webp" class="d-block w-100" alt="...">
 
-                              <div class="carousel-caption d-none d-md-block text">
+                              <div class="carousel-caption d-md-block text">
                                     <h2>SEA PACKAGE - ƯU ĐÃI PHÒNG/VILLA</h2>
                                     <p>Giá chỉ từ 6.000.000 VNĐ/3N2Đ. Tặng hotel credit lên tới 8.000.000 VNĐ.</p>
                                     <button><a href="">KHÁM PHÁ</a></button>
@@ -13,7 +13,7 @@
                         </div>
                         <div class="carousel-item">
                               <img src="./img/banner-homepage-1_1645412628.jpg.webp" class="d-block w-100" alt="...">
-                              <div class="carousel-caption d-none d-md-block text">
+                              <div class="carousel-caption d-md-block text">
                                     <h2>SEA PACKAGE - ƯU ĐÃI PHÒNG/VILLA</h2>
                                     <p>Giá chỉ từ 6.000.000 VNĐ/3N2Đ. Tặng hotel credit lên tới 8.000.000 VNĐ.</p>
                                     <button><a href="">KHÁM PHÁ</a></button>
@@ -31,7 +31,7 @@
                         </div>
                         <div class="carousel-item">
                               <img src="./img/Summer-Combo-Homepage-Banner_1652884783.png.webp" class="d-block w-100" alt="...">
-                              <div class="carousel-caption d-none d-md-block text">
+                              <div class="carousel-caption d-md-block text">
                                     <h2>SEA PACKAGE - ƯU ĐÃI PHÒNG/VILLA</h2>
                                     <p>Giá chỉ từ 6.000.000 VNĐ/3N2Đ. Tặng hotel credit lên tới 8.000.000 VNĐ.</p>
                                     <button><a href="">KHÁM PHÁ</a></button>
@@ -40,7 +40,7 @@
                         </div>
                         <div class="carousel-item">
                               <img src="./img/uu-dai-vinpearl-techcombank-homepage-Banner_1653554000.jpg.webp" class="d-block w-100" alt="...">
-                              <div class="carousel-caption d-none d-md-block text">
+                              <div class="carousel-caption d-md-block text">
                                     <h2>SEA PACKAGE - ƯU ĐÃI PHÒNG/VILLA</h2>
                                     <p>Giá chỉ từ 6.000.000 VNĐ/3N2Đ. Tặng hotel credit lên tới 8.000.000 VNĐ.</p>
                                     <button><a href="">KHÁM PHÁ</a></button>
@@ -49,7 +49,7 @@
                         </div>
                         <div class="carousel-item">
                               <img src="./img/discovery-bus-banner-hompage_1653383686.jpg.webp" class="d-block w-100" alt="...">
-                              <div class="carousel-caption d-none d-md-block text">
+                              <div class="carousel-caption d-md-block text">
                                     <h2>SEA PACKAGE - ƯU ĐÃI PHÒNG/VILLA</h2>
                                     <p>Giá chỉ từ 6.000.000 VNĐ/3N2Đ. Tặng hotel credit lên tới 8.000.000 VNĐ.</p>
                                     <button><a href="">KHÁM PHÁ</a></button>
@@ -120,33 +120,41 @@
                         </div>
                   </div>
             </div>
+            <div class="respon__slide">
+                  <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Khách sạn / Tour du lịch" aria-label="Recipient's username" aria-describedby="button-addon2">
+                        <div class="input-group-append">
+                              <button class="btn btn-outline-secondary" type="button" id="button-addon2">Tìm kiếm</button>
+                        </div>
+                  </div>
+            </div>
       </div>
 </div>
 <div class="nap container">
       <div class="nap__content">
             <div class="row">
-                  <div class="nap__item col-3">
+                  <div class="nap__item col-xl-3 col-lg-3 col-md-6 col-dm-6 col-6">
                         <div class="row">
                               <div class="col-3"><img src="./img/hand.png" alt=""></div>
                               <div class="col-9"><span>Đảm bảo<div>giá tốt nhất</div>
                                     </span></div>
                         </div>
                   </div>
-                  <div class="nap__item col-3">
+                  <div class="nap__item col-xl-3 col-lg-3 col-md-6 col-dm-6 col-6">
                         <div class="row">
                               <div class="col-3"> <img src="./img/bed.png" alt=""></div>
                               <div class="col-9"><span>Nâng hạng phòng<div>miễn phí
                                           </div></span></div>
                         </div>
                   </div>
-                  <div class="nap__item col-3">
+                  <div class="nap__item col-xl-3 col-lg-3 col-md-6 col-dm-6 col-6">
                         <div class="row">
                               <div class="col-3"><img src="./img/medal.png" alt=""></div>
                               <div class="col-9"><span>Chính sách<div>hoàn hủy linh
                                                 hoạt</div></span></div>
                         </div>
                   </div>
-                  <div class="nap__item col-3">
+                  <div class="nap__item col-xl-3 col-lg-3 col-md-6 col-dm-6 col-6">
                         <div class="row">
                               <div class="col-3"><img src="./img/back-in-time.png" alt=""></div>
                               <div class="col-9"><span>Thời gian<div>nhận trả phòng
@@ -161,14 +169,14 @@
       <h2>Ưu đãi - Khuyến mại</h2>
       <div class="offer__content">
             <div class="row">
-                  <div class="col-6" style="margin: 15px 0;">
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-dm-12 col-12" style="margin: 15px 0;">
                         <div class="offer__item">
                               <img src="./img/chuong-trinh-am-thuc-vinpearl-condotel-beachfront-nha-trang-banner_1656072358.jpg.webp" alt="">
                               <h3>Đón hè tuyệt nhất tại Vinpearl Condotel Beachfront Nha Trang cùng 10 chương trình ẩm thực hấp dẫn</h3>
                               <button><a href="">Xem chi tiết</a></button>
                         </div>
                   </div>
-                  <div class="col-6" style="margin: 15px 0;">
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-dm-12 col-12" style="margin: 15px 0;">
                         <div class="offer__item">
                               <img src="./img/chuong-trinh-am-thuc-vinpearl-discovery-banner_1656045162.jpg.webp" alt="">
                               <h3>8 chương trình ẩm thực đón hè trendy tại Vinpearl Discovery</h3>
@@ -176,14 +184,14 @@
                         </div>
                   </div>
 
-                  <div class="col-6" style="margin: 15px 0;">
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-dm-12 col-12" style="margin: 15px 0;">
                         <div class="offer__item">
                               <img src="./img/offer-at-vinpearl-luxury_1653382337.jpg.webp" alt="">
                               <h3>“Đa vũ trụ” trải nghiệm và ưu đãi hè cực đỉnh tại Vinpearl Luxury</h3>
                               <button><a href="">Xem chi tiết</a></button>
                         </div>
                   </div>
-                  <div class="col-6" style="margin: 15px 0;">
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-dm-12 col-12" style="margin: 15px 0;">
                         <div class="offer__item">
                               <img src="./img/uu-dai-vinpearl_1654769647.JPG.webp" alt="">
                               <h3>Ưu đãi cực chất – Vui hè cực đỉnh cùng Vinpearl tháng 6</h3>
@@ -237,9 +245,9 @@
                         <?php extract($ks) ?>
                         <div class="item">
                               <div class="card" style="width: 22rem;">
-                                    <a href="index.php?act=chitietks&&maks=<?=$maks?>&&makv=<?=$makv?>"><img src="./img/khachsan/<?= $anh ?>" class="card-img-top" alt="..."></a>
+                                    <a href="index.php?act=chitietks&&maks=<?= $maks ?>&&makv=<?= $makv ?>"><img src="./img/khachsan/<?= $anh ?>" class="card-img-top" alt="..."></a>
                                     <div class="card-body">
-                                          <a href="index.php?act=chitietks&&maks=<?=$maks?>&&makv=<?=$makv?>">
+                                          <a href="index.php?act=chitietks&&maks=<?= $maks ?>&&makv=<?= $makv ?>">
                                                 <h5 class="card-title"><?= $tenks ?></h5>
                                           </a>
                                           <div class="products__detail">
@@ -299,8 +307,8 @@
       <h2>Khu du lịch nổi tiếng</h2>
       <div class="location__content">
             <div class="row">
-                  <div class="col-4">
-                        <div class="location__item">
+                  <div class="col-xl-4 col-lg-6 col-md-12 col-dm-12 col-12 vip">
+                        <div class="location__item t1">
                               <img src="./img/Vinpearl-Nha-Trang-Diem-den-16x9-Hinh-anh-1_0.png.webp" alt="">
                               <div class="location__text">
                                     <h3>NHA TRANG</h3>
@@ -308,8 +316,8 @@
                               </div>
                         </div>
                   </div>
-                  <div class="col-4">
-                        <div class="location__item">
+                  <div class="col-xl-4 col-lg-6 col-md-12 col-dm-12 col-12 vip">
+                        <div class="location__item t2">
                               <img src="./img/867_PQUC_Thumbnail_điểm đến.jpeg.webp" alt="">
                               <div class="location__text">
                                     <h3>PHÚ QUỐC UNITED CENTER</h3>
@@ -317,8 +325,8 @@
                               </div>
                         </div>
                   </div>
-                  <div class="col-4">
-                        <div class="location__item">
+                  <div class="col-xl-4 col-lg-6 col-md-12 col-dm-12 col-12 vip">
+                        <div class="location__item t3">
                               <img src="./img/Vinpearl-Nam-Hoi-An-Diem-den-16x9-Hinh-anh-1.png.webp" alt="">
                               <div class="location__text">
                                     <h3>NAM HỘI AN</h3>
@@ -347,22 +355,22 @@
                   </div>
                   <div class="partner__right">
                         <div class="row">
-                              <div class="col-2">
+                              <div class="col-xl-2 col-lg-2 col-md-6 col-dm-6 col-6">
                                     <img src="img/vp_hotel_resort_1647925026.webp" alt="">
                               </div>
-                              <div class="col-2">
+                              <div class="col-2 col-xl-2 col-lg-2 col-md-6 col-dm-6 col-6">
                                     <img src="img/vinoasis_1647925040.webp" alt="">
                               </div>
-                              <div class="col-2">
+                              <div class="col-2 col-xl-2 col-lg-2 col-md-6 col-dm-6 col-6">
                                     <img src="img/vp_luxury_1647925062.webp" alt="">
                               </div>
-                              <div class="col-2">
+                              <div class="col-2 col-xl-2 col-lg-2 col-md-6 col-dm-6 col-6">
                                     <img src="img/condotel_1647925071.webp" alt="">
                               </div>
-                              <div class="col-2">
+                              <div class="col-2 col-xl-2 col-lg-2 col-md-6 col-dm-6 col-6">
                                     <img src="img/discovery_1647925078.webp" alt="">
                               </div>
-                              <div class="col-2">
+                              <div class="col-2 col-xl-2 col-lg-2 col-md-6 col-dm-6 col-6">
                                     <img src="img/holidays_1647925088.webp" alt="">
                               </div>
                         </div>
@@ -374,13 +382,13 @@
                   </div>
                   <div class="partner__right">
                         <div class="row">
-                              <div class="col-4">
+                              <div class="col-xl-2 col-lg-2 col-md-6 col-dm-6 col-6">
                                     <img src="./img/convention_1647925137.webp" alt="">
                               </div>
-                              <div class="col-4">
+                              <div class="col-xl-2 col-lg-2 col-md-6 col-dm-6 col-6">
                                     <img src="./img/almaz_1647925645.webp" alt="">
                               </div>
-                              <div class="col-4">
+                              <div class="col-xl-2 col-lg-2 col-md-6 col-dm-6 col-6">
                                     <img src="./img/imperia_1647925662.webp" alt="">
                               </div>
                         </div>
@@ -392,22 +400,22 @@
                   </div>
                   <div class="partner__right">
                         <div class="row">
-                              <div class="col-2">
+                              <div class="col-xl-2 col-lg-2 col-md-6 col-dm-6 col-6">
                                     <img src="img/vw_1647925754.svg" alt="">
                               </div>
-                              <div class="col-2">
+                              <div class="col-xl-2 col-lg-2 col-md-6 col-dm-6 col-6">
                                     <img src="img/vp_golf_1647925676.svg" alt="">
                               </div>
-                              <div class="col-2">
+                              <div class="col-xl-2 col-lg-2 col-md-6 col-dm-6 col-6">
                                     <img src="img/safari_1647925690.svg" alt="">
                               </div>
-                              <div class="col-2">
+                              <div class="col-xl-2 col-lg-2 col-md-6 col-dm-6 col-6">
                                     <img src="img/grand_world_1647925701.svg" alt="">
                               </div>
-                              <div class="col-2">
+                              <div class="col-xl-2 col-lg-2 col-md-6 col-dm-6 col-6">
                                     <img src="img/pquc_1647925710.svg" alt="">
                               </div>
-                              <div class="col-2">
+                              <div class="col-xl-2 col-lg-2 col-md-6 col-dm-6 col-6">
                                     <img src="img/holidays_1647925088.webp" alt="">
                               </div>
                         </div>
@@ -445,4 +453,5 @@
             })
       })
 </script>
+
 </style>
